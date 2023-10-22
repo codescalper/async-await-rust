018a18e27f6d61b252d2ae8fa7208fdf68ea59a5
codescalper <msfunbook@gmail.com>
2023-10-22 10:19:28 +0530

    Created rust async await req using tokio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use error_chain::error_chain;
+
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let res = reqwest::get("https://dummyjson.com/products/1").await?;
+    println!("Status: {}", res.status());
+    println!("Headers:\n{:#?}", res.headers());
+
+    let body = res.text().await?;
+    println!("Body:\n{:#?}", body);
+
+    println!("Hello Wolrd");
+    Ok(())
 }
