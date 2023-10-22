b06e96d4eb39abd2c4206a8d7a1f006c858c84cc
codescalper <msfunbook@gmail.com>
2023-10-22 10:43:10 +0530

    Added comments for better understanding

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 use error_chain::error_chain;
 
-error_chain! {
+error_chain! { //* Error chain macro */
     foreign_links {
         Io(std::io::Error);
         HttpRequest(reqwest::Error);
     }
 }
 
-#[tokio::main]
+#[tokio::main] // * THis sets up tokio runtime for asynchornous tasks
 async fn main() -> Result<()> {
     let res = reqwest::get("https://dummyjson.com/products/1").await?;
     println!("Status: {}", res.status());
     println!("Headers:\n{:#?}", res.headers());
 
-    let body = res.text().await?;
+    let body = res.text().await?; // * .text() method is asycnhronous
     println!("Body:\n{:#?}", body);
 
     println!("Hello Wolrd");
